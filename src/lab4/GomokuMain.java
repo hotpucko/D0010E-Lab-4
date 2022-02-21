4eda5507fad45828ffc8034a544b935ed20652ab
Hotpucko <stn.jonsson@gmail.com>
2022-02-21 19:08:15 +0100

    Modified GomokuMain: ~modified client initialization, changed to correct port

--- src/lab4/GomokuMain.java
+++ src/lab4/GomokuMain.java
@@ -8,9 +8,15 @@ public class GomokuMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		GomokuClient client = new GomokuClient(0);
+		GomokuClient client = new GomokuClient(4001);
 		GomokuGameState gameState = new GomokuGameState(client);
 		GomokuGUI gui = new GomokuGUI(gameState, client);
+		
+		/*
+		GomokuClient client2 = new GomokuClient(4001);
+		GomokuGameState gameState2 = new GomokuGameState(client);
+		GomokuGUI gui2 = new GomokuGUI(gameState, client);
+		*/
 	}
 
 }
