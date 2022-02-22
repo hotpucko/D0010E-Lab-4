a1e71f3396513e8bcf92cfcde0bae7ffa988a84e
Hotpucko <stn.jonsson@gmail.com>
2022-02-22 18:20:45 +0100

    Formatted GomokoGUI.java

--- src/lab4/gui/GomokuGUI.java
+++ src/lab4/gui/GomokuGUI.java
@@ -1,4 +1,5 @@
 package lab4.gui;
+
 import java.awt.Button;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -23,7 +24,7 @@ import lab4.data.GomokuGameState;
  * The GUI class
  */
 
-public class GomokuGUI implements Observer{
+public class GomokuGUI implements Observer {
 
 	private GomokuClient client;
 	private GomokuGameState gamestate;
@@ -31,32 +32,33 @@ public class GomokuGUI implements Observer{
 	private JButton newGameButton;
 	private JButton disconnectButton;
 	private Label messageLabel;
-	
+
 	private ConnectionWindow connectionWindow;
-	
+
 	/**
 	 * The constructor
 	 * 
-	 * @param g   The game state that the GUI will visualize
-	 * @param c   The client that is responsible for the communication
+	 * @param g
+	 *            The game state that the GUI will visualize
+	 * @param c
+	 *            The client that is responsible for the communication
 	 */
-	public GomokuGUI(GomokuGameState g, GomokuClient c){
+	public GomokuGUI(GomokuGameState g, GomokuClient c) {
 		this.client = c;
 		this.gamestate = g;
 		client.addObserver(this);
 		gamestate.addObserver(this);
-		
-		//initialize variables
+
+		// initialize variables
 		connectButton = new JButton("connect");
-		
+
 		ActionListener connectListener = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				connectionWindow = new ConnectionWindow(c);
-				System.out.println("conn");
 			}
 		};
 		connectButton.addActionListener(connectListener);
-		
+
 		newGameButton = new JButton("new game");
 		ActionListener newGameListener = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -64,7 +66,7 @@ public class GomokuGUI implements Observer{
 			}
 		};
 		newGameButton.addActionListener(newGameListener);
-		
+
 		disconnectButton = new JButton("disconnect");
 		ActionListener disconnectListener = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -72,70 +74,68 @@ public class GomokuGUI implements Observer{
 			}
 		};
 		disconnectButton.addActionListener(disconnectListener);
-		
+
 		messageLabel = new Label("text");
-		
-		
-		
-		//initialize panel
+
+		// initialize panel
 		GamePanel panel = new GamePanel(g.getGameGrid());
 		panel.setVisible(true);
-		//add listener
+		// add listener
 		MouseAdapter mouseAdapter = new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				int[] clickedPosition = panel.getGridPosition(e.getX(), e.getY());
 				g.move(clickedPosition[0], clickedPosition[1]);
-				System.out.println(String.format("clicked x: %s, y: %s, gridPos: x: %d, y: %d", e.getX(), e.getY(), clickedPosition[0], clickedPosition[1]));
 			}
 		};
 		panel.addMouseListener(mouseAdapter);
-		
-		//initialize frame
+
+		// initialize frame
 		JFrame frame = new JFrame();
 		frame.setLayout(new FlowLayout());
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(false);
-		
-		
-		
-		//handle UI structure
+
+		// handle UI structure
 		Box verticalBox = Box.createVerticalBox();
-		
+
 		Box buttonBox = Box.createHorizontalBox();
 		buttonBox.add(connectButton);
 		buttonBox.add(newGameButton);
 		buttonBox.add(disconnectButton);
-		
+
 		verticalBox.add(panel);
 		verticalBox.add(buttonBox);
 		verticalBox.add(messageLabel);
-		
+
 		frame.add(verticalBox);
 		frame.pack();
+		frame.setMinimumSize(frame.getSize());
+
+		newGameButton.setEnabled(false);
+		disconnectButton.setEnabled(false);
+
 	}
-	
-	
+
 	public void update(Observable arg0, Object arg1) {
-		
+
 		// Update the buttons if the connection status has changed
-		if(arg0 == client){
-			if(client.getConnectionStatus() == GomokuClient.UNCONNECTED){
+		if (arg0 == client) {
+			if (client.getConnectionStatus() == GomokuClient.UNCONNECTED) {
 				connectButton.setEnabled(true);
 				newGameButton.setEnabled(false);
 				disconnectButton.setEnabled(false);
-			}else{
+			} else {
 				connectButton.setEnabled(false);
 				newGameButton.setEnabled(true);
 				disconnectButton.setEnabled(true);
 			}
 		}
-		
+
 		// Update the status text if the gamestate has changed
-		if(arg0 == gamestate){
+		if (arg0 == gamestate) {
 			messageLabel.setText(gamestate.getMessageString());
 		}
-		
+
 	}
-	
+
 }
